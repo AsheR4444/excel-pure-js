408fcb218e434b6c1a24535958a3b8622171eebb
Ilya <il08082000@gmail.com>
2021-10-27 01:21:27 +0300

    [excel/task/formirovanie-verstki]

--- src/components/Excel/Excel.js
+++ src/components/Excel/Excel.js
@@ -1,3 +1,5 @@
+import { $ } from '../../core/dom';
+
 export class Excel {
     constructor(selector, options) {
         this.$el = document.querySelector(selector);
@@ -5,17 +7,21 @@ export class Excel {
     }
 
     getRoot() {
-        const $root = document.createElement('div');
+        const $root = $.create('div', 'excel');
+
         this.components.forEach((Component) => {
-            const component = new Component();
+            const $el = $.create('div', Component.className);
+
+            const component = new Component($el);
+            $el.innerHTML = component.toHTML();
+
+            $root.append($el);
         });
 
         return $root;
     }
 
     render() {
-        // this.$el.insertAdjacentHTML('afterbegin', '<h1>Test</h1>');
-
         this.$el.append(this.getRoot());
     }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,1 +1,9 @@
-export class DomListener {}
+export class DomListener {
+    constructor($root) {
+        if (!$root) {
+            throw new Error('No $root provided');
+        }
+
+        this.$root = $root;
+    }
+}
